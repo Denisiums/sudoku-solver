3a84494b78a50adf99d7fb02f73d6c59120e3e65
d.ilyukhin <d.ilyukhin@distillery.com>
2017-03-13 13:20:53 +0300

    Basic calculation

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ window.onload = function() {
 			this.y = y;
 			// this.areaKey = area || null;
 			this.locked = (typeof this.locked !== 'undefined') ? locked : false;
-			this.denied = [];
-		}
-	}
-
-	class Area {
-		constructor() {
-			this.elements = [];
+			this.banned = [];
 		}
 
-		checkElement(element) {
-			let elements = this.elements;
-			return !(elements.some((item, index) => {
-				return element.value === item.value;
-			}));
+		isBanned(value) {
+			if (isNaN(value) || value === null) return true;
+			return (this.banned.indexOf(value) !== -1);
+		}
+
+		ban(value) {
+			this.banned.push(value);
+		}
+
+		unbanAll() {
+			this.banned = [];
 		}
 	}
 
@@ -29,7 +29,9 @@ window.onload = function() {
 		constructor(grid) {
 			this.grid = grid || Sudoku.getEmptyGrid();
 			this.numbers = this.generateNumbers();
-			// this.areas = {};
+			this.currentElement = null;
+			this.finished = false;
+			this.lastMove = 'forward'; // forward || back
 		}
 
 		static getEmptyGrid() {
@@ -37,7 +39,7 @@ window.onload = function() {
 			for (let i = 0; i < 9; i++) {
 				const row = [];
 				for (let j = 0; j < 9; j++) {
-					row.push(new Element(null, i, j, 'area'));
+					row.push(new Element(null, j, i, false));
 				}
 				result.push(row);
 			}
@@ -45,27 +47,100 @@ window.onload = function() {
 		}
 
 		init() {
-			this.generateAreas();
 
 		}
-		//
-		// generateAreas() {
-		// 	for (let i = 0; i < 9; i++) {
-		// 		this.areas[i] = new Area();
-		// 	}
-		// }
-		//
-		// setElementArea() {
-		// 	const keys = this.areas.keys;
-		//
-		// 	for (let key of keys) {
-		// 		if (!Object.hasOwnProperty(key)) return;
-		// 		const area = this.areas[key];
-		//
-		//
-		// 	}
-		// }
 
+		start() {
+			this.currentElement = this.grid[0][0];
+			this.tryNextValue();
+		}
+
+		finish() {
+			console.log('Finished! ', this.grid);
+			this.finished = true;
+			drawSudoku(this.grid);
+		}
+
+		goForward() {
+			this.lastMove = 'forward';
+
+			if (!this.currentElement) {
+				return this.currentElement = this.grid[0][0];
+			}
+
+			if (this.currentElement.x < 8) {
+				this.currentElement = this.grid[this.currentElement.y][this.currentElement.x + 1];
+				return;
+			}
+
+			if (this.currentElement.y < 8) {
+				this.currentElement = this.grid[this.currentElement.y + 1][0];
+				return;
+			}
+
+			this.finish();
+
+		}
+
+		goBack() {
+			this.lastMove = 'back';
+
+			if (!this.currentElement) {
+				return this.currentElement = this.grid[0][0];
+			}
+
+			this.currentElement.unbanAll();
+			if (this.currentElement.x > 0) {
+				return this.currentElement = this.grid[this.currentElement.y][this.currentElement.x - 1];
+			}
+
+			if (this.currentElement.y > 0) {
+				return this.currentElement = this.grid[this.currentElement.y - 1][8];
+			}
+
+			this.finish();
+		}
+
+		tryNextValue() {
+			if (this.currentElement.locked) {
+				console.log('Locked!');
+				if (this.lastMove === 'back') {
+					this.goBack();
+				} else {
+					this.goForward();
+				}
+				this.tryNextValue();
+			}
+
+			if (this.currentElement.value !== null) {
+				this.currentElement.ban(this.currentElement.value);
+				this.currentElement.value = null;
+			}
+
+			const newValue = this.getNextValue();
+
+			if (!newValue) {
+				this.goBack();
+			} else {
+				this.currentElement.value = newValue;
+				this.goForward();
+			}
+
+			if (!this.finished) this.tryNextValue();
+		}
+
+		getNextValue() {
+			if (!this.currentElement) return null;
+			const numbers = this.getNumbers();
+			let result = null;
+			numbers.some((value) => {
+				if (this.checkValue(this.currentElement, value) && !this.currentElement.isBanned(value)) {
+					result = value;
+					return true;
+				}
+			});
+			return result;
+		}
 
 		setValue(x, y, value) {
 			if (isNaN(value) || !this.isInGrid(x) || !this.isInGrid(y)) return;
@@ -77,40 +152,42 @@ window.onload = function() {
 			this.grid[y][x].locked = true;
 		}
 
+
+
 		unlockValue(x, y) {
 			if (!this.isInGrid(x) || !this.isInGrid(y)) return;
 			this.grid[y][x].locked = false;
 		}
 
-		checkRow(element) {
-			let row = element.y;
-			return !(this.grid[row].some((item, index) => {
-				return element.value === item;
+		checkRow(element, value) {
+			let row = this.getElementRow(element);
+			return !(row.some((item) => {
+				return item.value === value;
 			}));
 		}
 
-		checkColumn(element) {
-			let column = element.x;
-			return !this.grid.some((row, index) => {
-				return element.value === row[column];
+		checkColumn(element, value) {
+			const column = this.getElementColumn(element);
+			return !column.some((item) => {
+				return item.value === value;
 			});
 		}
 
-		checkArea(element) {
+		checkArea(element, value) {
 			const area = this.getElementArea(element);
-			return !(area.some((item, index) => {
-				return element.value === item.value;
+			return !(area.some((item) => {
+				return item.value === value;
 			}));
 		}
 
-		cleckDiagonals() {
+		checkDiagonals() {
 			// TODO
 		}
 
-		checkElement(element) {
-			return this.checkRow(element)
-				&& this.checkColumn(element)
-				&& this.checkArea(element);
+		checkValue(element, value) {
+			return this.checkRow(element, value)
+				&& this.checkColumn(element, value)
+				&& this.checkArea(element, value);
 		}
 
 		getElementArea(element) {
@@ -120,10 +197,23 @@ window.onload = function() {
 			const cornerY = Math.floor(element.y / 3) * 3;
 			for (let i = 0; i < 3; i++) {
 				for (let j = 0; j < 3; j++) {
-					result.push(this.grid[i][j]);
+					result.push(this.grid[cornerY + i][cornerX + j]);
 				}
 			}
 			return result;
+		}
+
+		getElementRow(element) {
+			if (!element) return;
+			return [...this.grid[element.y]]
+		}
+
+		getElementColumn(element) {
+			if (!element) return;
+			const x = element.x;
+			return this.grid.map(row => {
+				return row[x]
+			})
 		}
 
 		isInGrid(coordinate) {
@@ -154,27 +244,25 @@ window.onload = function() {
 			return [...this.numbers];
 		}
 
-		getPossibleRowValues(y) {
-			if (!this.isInGrid(y)) return [];
-			const row = [...this.grid[y]];
-			const result = this.getNumbers();
-			row.forEach(item => {
-				const index = result.indexOf(item);
-				if (index !== -1) {
-					result.splice(index, 1);
-				}
-			});
-			return result;
-		}
-
-
-
-
-
 	}
 
 	const sudoku = new Sudoku();
 	sudoku.init();
+
+	document.getElementById('start').addEventListener('click', function() {
+		sudoku.start();
+	});
+
+	function drawSudoku(grid) {
+		let text = '';
+		grid.forEach(row => {
+			row.forEach(element => {
+				text += `${element.value || 'null'}, `;
+			});
+			text += '<br>'
+		});
+		document.getElementById('sudoku').innerHTML = text;
+	}
 
 	console.log('sudoku: ', sudoku);
 
